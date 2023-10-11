ea1d9b33a79b624c94c3b7cd191e668c749310d9
Salomao <salomao.machado.75@gmail.com>
2023-10-11 20:53:11 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '001_11-10-23_jogo-da-velha';
+}
